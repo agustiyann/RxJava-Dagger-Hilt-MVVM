1e004bf44dd7002bd500fa52b1712cc8287e5410
agustiyann <agustiyaninfo@gmail.com>
2020-11-20 10:29:07 +0700

    Initial commit

---
+++ app/src/main/java/com/masscode/gonews/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.masscode.gonews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
