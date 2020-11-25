382257b1b647fbf357a7a388831990106e3b0e25
agustiyann <agustiyaninfo@gmail.com>
2020-11-25 14:13:32 +0700

    Implement hilt jetpack for viewmodel

--- app/src/main/java/com/masscode/gonews/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/masscode/gonews/ui/home/HomeViewModel.kt
@@ -1,10 +1,10 @@
 package com.masscode.gonews.ui.home
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
 import com.masscode.gonews.domain.usecase.UserUseCase
-import javax.inject.Inject
 
-class HomeViewModel @Inject constructor(userUseCase: UserUseCase) : ViewModel() {
+class HomeViewModel @ViewModelInject constructor(userUseCase: UserUseCase) : ViewModel() {
     val articles = LiveDataReactiveStreams.fromPublisher(userUseCase.getAllArticles())
 }
